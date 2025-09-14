6fa96ea30f170341438df6a7cedcf98d5e2370dc
Tanu Bhardwaj <bhardwajtanu059@gmail.com>
2025-09-14 17:52:24 +0530

    added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup
+
+setup(
+    name= "medical chatbot",
+    version = "0.1.0",
+    author = "Tanu Bhardwaj",
+    author_email = "bhardwajtanu059@hmail.com",
+    packages = find_packages(),
+)
